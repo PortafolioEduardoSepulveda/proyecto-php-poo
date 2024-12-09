a28c55eb1d3d3264f883bdf1d23529dcc1e1b0ac
Eduardo <eduardo.patricio@gmail.com>
2024-12-09 00:16:23 -0300

    commit 1,0

---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Desarrollado por Eduardo Sepulveda WEB &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
